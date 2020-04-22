1b434fdc781029ff9218af0c6e7b7eb9fb856849
7IN0SAN9 <me@7in0.me>
2020-04-23 02:06:41 +0800

    Add EvaluationTests

---
+++ tests/LamiaSharp.Tests/EvaluationTests.cs
@@ -0,0 +1,30 @@
+﻿using LamiaSharp.Values;
+using Xunit;
+
+namespace LamiaSharp.Tests
+{
+    public class EvaluationTests
+    {
+        [Fact]
+        public void TestEvaluateConstant()
+        {
+            var constant = new Integer(42);
+
+            Assert.Equal(constant, constant.Evaluate(new Environment()));
+            Assert.Equal(42L, constant.Source);
+        }
+
+        [Fact]
+        public void TestEvaluateSymbol()
+        {
+            var env = new Environment();
+
+            var constant = new Integer(42);
+            var symbol = new Symbol("n");
+
+            env[symbol.Source] = constant;
+
+            Assert.Equal(constant, symbol.Evaluate(env));
+        }
+    }
+}
